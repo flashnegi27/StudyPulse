17b218015d5c7683e50e5025c62c531d8274a625
flashnegi27 <flashnegi27@gmail.com>
2026-08-08 23:50:42 +0530

    Delete app/src directory

--- app/src/main/java/com/studypulse/app/domain/model/SessionState.kt
+++
@@ -1,9 +0,0 @@
-package com.studypulse.app.domain.model
-
-sealed class SessionState {
-    object Idle : SessionState()
-    data class Studying(
-        val locationId: String,
-        val startTime: Long
-    ) : SessionState()
-}
--- app/src/main/java/com/studypulse/app/presentation/history/HistoryUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.studypulse.app.presentation.history
-
-import com.studypulse.app.domain.model.StudySession
-
-data class HistoryUiState(
-    val sessions:  List<StudySession> = emptyList(),
-    val isLoading: Boolean            = false
-)
